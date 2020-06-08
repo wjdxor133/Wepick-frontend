7da6aca37043900b736afce2a8187535a2976fce
권미란 <gwon.rran@gmail.com>
2020-06-08 23:04:25 +0900

    [Fix] Text

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from "./Routes";
-import "./styles/common.css";
 import "./styles/reset.css";
+import "./styles/common.css";
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
