5e712ef8536aff05d95defd8e69b2be65ca98285
권미란 <gwon.rran@gmail.com>
2020-06-12 16:27:52 +0900

    [Fix] main change

---
+++ src/pages/MainPage/MainImg.js
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+
+const MainImg = props => {
+
+    return (
+        <MainImgList>
+            <img src={props.img} alt="" />
+            <MainImgBox>
+                <MainWhiteBox>
+                    <span>{props.title}</span>
+                    <p>{props.content}</p>
+                    <div className="GoLink">바로가기 ></div>
+                </MainWhiteBox>
+            </MainImgBox>
+        </MainImgList>
+    );
+}
+
+const MainImgList = styled.li`
+    img{
+        width: 100%;
+        position: relative;
+    }
+   
+`;
+
+const MainImgBox = styled.div`
+    /* max-width: 1000px; */
+`;
+
+const MainWhiteBox = styled.div`
+    position: absolute;
+    z-index: 10;
+    top: 8em;
+    margin-left: calc((100vw - 1060px) / 2);
+    background-color: white;
+    width: 340px;
+    height: 150px;
+    position: absolute;
+    border-radius: .5em;
+    padding: 2em;
+    span{
+        font-size: 1.5rem;
+        font-weight: 600;
+    }
+    p{
+        font-size: .875rem;
+        font-weight: 600;
+        color: #666666;
+        margin-top: .7em;
+    }
+    .GoLink{
+        color: ${props => props.theme.color.main};
+        font-size: .875rem;
+        font-weight: 600;
+        margin-top: 1.5em;
+        padding-top: 1em;
+        border-top: 1px solid #dddddd;
+    }
+`;
+
+export default MainImg;
--- src/pages/MainPage/PositionList.js
+++ src/pages/MainPage/PositionList.js
@@ -1,15 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import { AiFillHeart } from 'react-icons/ai';
 
 const PositionList = (props) => {
-    // const [position, setposition] = useState({});
-    // console.log(props)
     return (
         <PositionBoxList >
-            <PositionImg><img src={props.img} alt="" /></PositionImg>
+            <PositionImg>
+                <img src={props.img} alt="" />
+                <LikeBox><div><AiFillHeart /></div><span>{props.like}</span> </LikeBox>
+            </PositionImg>
             <PositionListTitle> {props.title} </PositionListTitle>
-            <p> {props.name} </p>
-            <p> {props.area} </p>
+            <PositionText>
+                <p> {props.name} </p>
+                <p> {props.area} </p>
+            </PositionText>
             <Compensation>채용보상금 {props.compensation}원</Compensation>
         </PositionBoxList>
     );
@@ -27,6 +31,7 @@ const PositionBoxList = styled.li`
 `;
 
 const PositionImg = styled.div`
+  position: relative;
   width: 100%; 
   img{
       width: 100%;
@@ -34,17 +39,48 @@ const PositionImg = styled.div`
   }
 `;
 
-const PositionListTitle = styled.div`
-  
+const LikeBox = styled.div`
+  top: .7em;
+  right: .7em;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba( 0, 0, 0, 0.3);
+  color: white;
+  width: 60px;
+  height: 30px;
+  border-radius: .2em;
+  line-height: .1em;
+  div{
+      opacity: .5;
+      margin-right: .2em;
+  }
+  span{
+      font-size: .875rem;
+      font-weight: 600;
+  }
 `;
 
-/* const PostionListText = styled.div`
+const PositionListTitle = styled.div`
+  font-size: 1.25rem;
+  font-weight: 600;
+  color: ${props => props.theme.color.font};
+  margin: .8em 0px;
+`;
 
-`; */
+const PositionText = styled.div`
+    p{
+        color: #999999;
+        font-size: 1rem;
+        margin-bottom: .5em;
+    }
+`;
 
 const Compensation = styled.div`
   color: #666666;
-  font-size: .75rem;
+  font-size: .875rem;
+  margin-top: 1.2em;
 `;
 
 
